e17f208d072a74911f4096ed3425c6205b638888
Synobal <rbh2@hw.ac.uk>
2022-02-03 14:50:35 +0000

    update v2 - spacing/ifstatement
    if statement readabiity accessibilty increase

--- v2/v2.ino
+++ v2/v2.ino
@@ -7,7 +7,7 @@
 #define paramB 0.1
 #define paramMode 2
 
-#define paramBlockSpace 
+#define paramBlockSpace
 
 //GND
 //V5
@@ -16,7 +16,7 @@ int paramPulses;
 
 
 void setup() {
- 
+
 pinMode(SignalA, OUTPUT);
 pinMode(SignalB, OUTPUT);
 
@@ -27,80 +27,82 @@ pinMode(pushButton2, INPUT);//mode selection button
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
- paramA =0.8; //non const wait period for signal to be high
- paramPulses =13; //non const amount of pulses in required loop
+void loop() 
+{
+// put your main code here, to run repeatedly:
+paramA =0.8; //non const wait period for signal to be high
+paramPulses =13; //non const amount of pulses in required loop
 
 //instatiating pushbutton booleans
-int pushButton2Boolean=0; 
+int pushButton2Boolean=0;
 int pushButton1Boolean=0;
 
 
 //reading both digital values
-  pushButton2Boolean=digitalRead(pushButton2);
-  pushButton1Boolean=digitalRead(pushButton1);
+pushButton2Boolean=digitalRead(pushButton2);
+pushButton1Boolean=digitalRead(pushButton1);
 
 
-  Serial.print("push button 2");
-  Serial.print(pushButton1Boolean);
+Serial.print("push button 2");
+Serial.print(pushButton1Boolean);
 
-  Serial.print("push button 1");
-  Serial.print(pushButton1Boolean);
-  
-  
-  if (pushButton1Boolean==1)
-  {
-    //disable stream of pulses
-    return;
-  }
-  
+Serial.print("push button 1");
+Serial.print(pushButton1Boolean);
 
-  
-  else
-  {
-    
-    if (pushButton2Boolean) //this is the condition for the mode selection, 
-{
-  paramPulses=paramPulses+3; // if the button is presse then it adds 3 pulses to the amount of iterations required
-}else
-{
-  paramPulses=13;
-}
-    
-    digitalWrite(SignalB, HIGH); //starting signal
-    delay(0.5); //starting signal
-    digitalWrite(SignalB, LOW);//starting signal
 
-    
-    for (int i = 1; i <= paramPulses; i++) {
-    if (i==1) //since first iteration doesn't need 0.5milliseconds added, it is split into a if/else
+if (pushButton1Boolean==1)
     {
-    digitalWrite(SignalA, HIGH);
-      
-  delay(paramA);
-  
-  digitalWrite(SignalA, LOW);
-  delay(paramB);
-      
-    }else{
-      
-    
-    paramA=paramA+0.5; //adding 0.5milisec for each iteration
-    digitalWrite(SignalA, HIGH);
-      
-  delay(paramA);
-  
-  digitalWrite(SignalA, LOW);
-  delay(paramB);
-   }
+        //disable stream of pulses
+        return;
+    }
+else
+    {
 
-  }
-  
+    if (pushButton2Boolean) //this is the condition for the mode selection,
+        {
+            paramPulses=paramPulses+3; // if the button is presse then it adds 3 pulses to the amount of iterations required
+        }
+        else
+            {
+                paramPulses=13;
+            }
 
- 
-  }
- 
+            digitalWrite(SignalB, HIGH); //starting signal
+            delay(0.5); //starting signal
+            digitalWrite(SignalB, LOW);//starting signal
 
-  
+
+            for (int i = 1; i <= paramPulses; i++) 
+            {
+                if (i==1) //since first iteration doesn't need 0.5milliseconds added, it is split into a if/else
+                    {
+                        digitalWrite(SignalA, HIGH);
+
+                        delay(paramA);
+
+                        digitalWrite(SignalA, LOW);
+                        delay(paramB);
+
+                      }
+                      else
+                      {
+
+
+                        paramA=paramA+0.5; //adding 0.5milisec for each iteration
+                        digitalWrite(SignalA, HIGH);
+
+                        delay(paramA);
+
+                        digitalWrite(SignalA, LOW);
+                        delay(paramB);
+                       } 
+
+            }
+
+
+
+    }
+
+
+
 }
