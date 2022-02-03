f82c7d19eb8d1a335aa24c4ef699312284043170
Synobal <rbh2@hw.ac.uk>
2022-02-03 14:45:57 +0000

    update v2 - comments
    adding comments to the lines for documentation

--- v2/v2.ino
+++ v2/v2.ino
@@ -16,29 +16,31 @@ int paramPulses;
 
 
 void setup() {
-  // put your setup code here, to run once:
+ 
 pinMode(SignalA, OUTPUT);
 pinMode(SignalB, OUTPUT);
-pinMode(pushButton1, INPUT);
-pinMode(pushButton2, INPUT);
 
-//int pushButton2Boolean=0;
-//int pushButton1Boolean=0;
+pinMode(pushButton1, INPUT); //disable stream pulse button
+pinMode(pushButton2, INPUT);//mode selection button
+
 
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
- paramA =0.8;
- paramPulses =13;
+ paramA =0.8; //non const wait period for signal to be high
+ paramPulses =13; //non const amount of pulses in required loop
 
-
-int pushButton2Boolean=0;
+//instatiating pushbutton booleans
+int pushButton2Boolean=0; 
 int pushButton1Boolean=0;
 
+
+//reading both digital values
   pushButton2Boolean=digitalRead(pushButton2);
   pushButton1Boolean=digitalRead(pushButton1);
+
 
   Serial.print("push button 2");
   Serial.print(pushButton1Boolean);
@@ -58,21 +60,21 @@ int pushButton1Boolean=0;
   else
   {
     
-    if (pushButton2Boolean) //this is the condition for the mode selection
+    if (pushButton2Boolean) //this is the condition for the mode selection, 
 {
-  paramPulses=paramPulses+3;
+  paramPulses=paramPulses+3; // if the button is presse then it adds 3 pulses to the amount of iterations required
 }else
 {
   paramPulses=13;
 }
     
-    digitalWrite(SignalB, HIGH);
-    delay(0.5);
-    digitalWrite(SignalB, LOW);
+    digitalWrite(SignalB, HIGH); //starting signal
+    delay(0.5); //starting signal
+    digitalWrite(SignalB, LOW);//starting signal
 
     
     for (int i = 1; i <= paramPulses; i++) {
-    if (i==1)
+    if (i==1) //since first iteration doesn't need 0.5milliseconds added, it is split into a if/else
     {
     digitalWrite(SignalA, HIGH);
       
@@ -84,7 +86,7 @@ int pushButton1Boolean=0;
     }else{
       
     
-    paramA=paramA+0.5;
+    paramA=paramA+0.5; //adding 0.5milisec for each iteration
     digitalWrite(SignalA, HIGH);
       
   delay(paramA);
